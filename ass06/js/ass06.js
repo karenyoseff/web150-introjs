7defb01a27f69674e4e0e36a2679ab0d6b253a40
karenyoseff <52435963+karenyoseff@users.noreply.github.com>
2020-05-28 22:39:53 -0700

    Add files via upload

--- ass06/js/ass06.js
+++ ass06/js/ass06.js
@@ -2,11 +2,14 @@
 
 // Create a new element
 //Save it in a variable
-var newItem= document.createElement('li');
-// Create a text node.
-// Save it in as variable
-var newMenu = document.createTextNode('Caesar Salad with sardines');
+var newItem = document.createElement('li');
+//Setting bold style to the firsts words of the text
+var boldItem = document.createElement('b');
+boldItem.innerText = 'Caesar Salad';
+// Creating the rest of the text without bold styling
+var newMenu = document.createTextNode(' with sardines');
 // Attach text node we created
+newItem.appendChild(boldItem);
 newItem.appendChild(newMenu);
 // Set position where to add the element
 var position = document.getElementsByTagName('ul')[0];
@@ -16,14 +19,25 @@ position.appendChild(newItem);
 newItem.className = 'salad'
 
 
-// - Changing class attribute for appetizers
-var firstI = document.getElementById('p1');
-firstI.className = 'appetizers';
-var secondI = document.getElementById('p2');
-secondI.className = 'appetizers';
+// - Changing class attributes for items in the menu to get styles
+var items = document.getElementsByTagName('li');
+items[0].className = 'appetizers';
+items[1].className = 'appetizers';
+items[2].className = 'pasta';
+items[3].className = 'pasta';
+items[4].className = 'pasta';
 
+// items[5].className = 'pasta';      //This is an "unavailable" items
 
-// - Adding an attribute to item'Greek Salad'
-var saladItem = document.getElementsByTagName('li').item(6); //Get the item by the number of the list
-saladItem.setAttribute('class', 'salad'); // Add the attribute class
+// // - Adding an id attribute to item 'Greek Salad'
+var lastItem = document.getElementsByTagName('li')[7];
+lastItem.setAttribute("id", "p8")
 
+//Creating a loop for seasonal items in the menu by month, so depending on the month it will add one item
+var today = new Date(); //creating a var for a new date
+var month = today.getMonth(); //getting the current month
+if (month === 4) { // Assigning the conditional to May(4)
+    // - Adding an attribute to item'Greek Salad'
+    var saladItem = document.getElementsByTagName('li').item(6); //Get the item by the number of the list
+    saladItem.setAttribute('class', 'salad'); // Add the attribute class
+}
